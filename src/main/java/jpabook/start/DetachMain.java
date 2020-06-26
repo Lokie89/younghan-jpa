8a1bdd1c10fa5b3c39dd09e8d5d7ca5d3108988f
Lokie89 <traeuman@gmail.com>
2020-06-26 17:56:23 +0900

    3-6 준영속

---
+++ src/main/java/jpabook/start/DetachMain.java
@@ -0,0 +1,34 @@
+package jpabook.start;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/**
+ * @author lokie on 2020/06/25
+ */
+public class DetachMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("jpabook");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            Member member = new Member();
+            member.setId("memberA");
+            member.setUsername("회원A");
+
+            em.persist(member);
+
+            em.detach(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
