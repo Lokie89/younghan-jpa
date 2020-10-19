e85b68d4590baa0c3ff1fca310004f721f0bc0fa
Lokie89 <traeuman@gmail.com>
2020-10-19 10:39:13 +0900

    5-3 양방향 연관관계

--- src/main/java/jpabook/start/Team.java
+++ src/main/java/jpabook/start/Team.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
@@ -13,6 +16,9 @@ public class Team {
     @Column(name = "TEAM_ID")
     private String id;
     private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<Member>();
 
     public Team(String id, String name) {
         this.id = id;
