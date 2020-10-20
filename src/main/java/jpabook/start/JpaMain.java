23a87fcdfc0cbf7750995204c89341d3cba497e5
Lokie89 <traeuman@gmail.com>
2020-10-20 10:19:45 +0900

    5-6 양방향 연관관계의 주의점

--- src/main/java/jpabook/start/JpaMain.java
+++ src/main/java/jpabook/start/JpaMain.java
@@ -64,6 +64,44 @@ public class JpaMain {
         em.persist(member2);
     }
 
+    public void testSaveNonOwner(EntityManager em) {
+
+        // 회원1 저장
+        Member member1 = new Member("member1", "회원1");
+        em.persist(member1);
+
+        // 회원2 저장
+        Member member2 = new Member("member2", "회원2");
+        em.persist(member2);
+
+        Team team1 = new Team("team1", "팀1");
+        // 주인이 아닌 곳만 연관관계 설정
+        team1.getMembers().add(member1);
+        team1.getMembers().add(member2);
+
+        em.persist(team1);
+    }
+
+    public void testORM_양방향(EntityManager em) {
+
+        // 팀1 저장
+        Team team1 = new Team("team1", "팀1");
+        em.persist(team1);
+
+        Member member1 = new Member("member1", "회원1");
+
+        // 양방향 연관관계 설정
+        member1.setTeam(team1); // 연관관계 설정 member1 -> team1
+        team1.getMembers().add(member1); // 연관관계 설정 team1 -> member1
+        em.persist(member1);
+
+        Member member2 = new Member("member2", "회원2");
+        // 양방향 연관관계 설정
+        member2.setTeam(team1); // 연관관계 설정 member2 -> team1
+        team1.getMembers().add(member2); // 연관관계 설정 team1 -> member2
+        em.persist(member2);
+    }
+
     private static void queryLogicJoin(EntityManager em) {
         String jpql = "select m from Member m join m.team t where t.name = :teamName";
 
